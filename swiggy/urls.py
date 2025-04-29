c266947a59c5749fb30cf3074ef15dfe865b474a
bhavyagowda18 <gowdabhavya2003@gmail.com>
2025-04-29 04:06:09 -0500

    second

--- swiggy/urls.py
+++ swiggy/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from food.views import*
+from instamart.views import*
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cake/',cake,name='cake'),
+    path('booking/',booking,name='booking'),
 ]
